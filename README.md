1508b08490889d9dec824815a89dbff8061daf91
David Karasek <idjk@me.com>
2018-08-12 18:39:43 -0500

    added all the files

---
+++ README.md
@@ -0,0 +1,49 @@
+This program runs a server that listens to two different crypto exchanges websockets and displays the data in real time on a basic webpage.
+
+#How to run
+
+Clone the project.
+
+Setup a python3 virtual environment. This was developed against Python 3.7, but should be compatible back to 3.5.3.
+
+You will need python3, pip and virtualenv installed.
+
+Setup virtualenv
+
+`python3 -m venv {env}`
+
+{env} can be anything you like. Even just env.
+
+Start the virtualenv
+
+`. {env}/bin/activate`
+
+{env} being the same as above.
+
+Next, install dependencies. 
+
+`pip install -r requirements.txt`
+
+To run the server,
+
+`python3 server.py`
+
+Now the page will be hosted at `localhost:5000`
+
+If everything went as planned, once you navigate to `localhost:5000` you will see a basic page which will start blank. But after a few seconds should begin populating data in real time. 
+
+#The Problem
+
+`Display a running list of the time (in UTC), best bid, best ask, and the last traded price on any pair on two different crypto exchanges.`
+
+The problem as I approached it was to listen to two different websocket streams and also update the information in real time on a basic web page. I created a base class ExchangeListener and extended a child class for each exchange. This is because each exchange has different requirements and it makes it easier to expand functionality and add additional exchanges.
+
+Alternatively I could have setup an event loop with asyncio and polled the RESTapi. 
+
+For the front end I chose to keep it simple and use a basic flask application. This uses socketIO to pass data back and forth from the server to the page. I tossed in bootstrap to make it slightly better on the eyes with minimal effort. But it is otherwise overkill for this simple project at it's current state. 
+
+### Enhancements
+
+Navigating to the page initially shows a black page. This could be mitigated by either storing the collected data in a database or making calls to the rest API to back fill the data if desired.
+
+For the front end, adding d3 would have been a fairly simple next step to better display and organize the data.
---
+++ server.py
@@ -0,0 +1,126 @@
+import websocket
+import ssl
+import asyncio
+import json
+from threading import Thread
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+from datetime import datetime
+from dotenv import load_dotenv
+import os
+
+#load env variables for flask secret key
+load_dotenv()
+
+class ExchangeListener():
+    url = ""
+    exchange = ""
+
+    def __init__(self, exchange, url):
+        self.url = url
+        self.exchange = exchange
+
+    def on_message(self, ws, message):
+        print("--> {} message <--".format(self.exchange))
+
+    def on_error(self, ws, error):
+        print("--> {} Error! <--".format(self.exchange))
+        print(error)
+
+    def on_close(self, ws):
+        print("--> {} closed <--".format(self.exchange))
+
+    def on_open(self, ws):
+        print("--> {} connection opened <--".format(self.exchange))
+
+    def connect(self):
+        # exit if nothing to connect to
+        if self.url == "": return
+        
+        ws = websocket.WebSocketApp(self.url, 
+                                    on_message = self.on_message,
+                                    on_error = self.on_error,
+                                    on_close = self.on_close)
+        ws.on_open = self.on_open
+        ws.run_forever(sslopt={"cert_reqs": ssl.CERT_NONE})
+
+class Hitbtc(ExchangeListener):
+
+    url = "wss://api.hitbtc.com/api/2/ws"
+    exchange = "hitbtc"
+
+    def __init__(self):
+        print("Initializing {}".format(self.exchange))
+
+    def on_open(self, ws):
+        print("--> {} override open <--".format(self.exchange))
+        ws.send(json.dumps({"method":"subscribeTicker","params":{"symbol": "LTCBTC"}, "id": 1011}))
+
+    def on_message(self, ws, message):
+        print("--> {} override message <--".format(self.exchange))
+        msg = json.loads(message)
+        ticker = {
+            "time": msg["params"]["timestamp"],
+            "best_ask": msg["params"]["ask"],
+            "best_bid": msg["params"]["bid"],
+            "last_trade": msg["params"]["last"]
+        }
+        socketio.emit('tick', {'exchange':self.exchange, 'data': ticker, 'type': 'ticker'}, namespace='/ticker')
+
+class Bitfinex(ExchangeListener):
+
+    url = "wss://api.bitfinex.com/ws/2"
+    exchange = "bitfinex"
+
+    def __init__(self):
+        print("Initializing {}".format(self.exchange))
+
+    def on_open(self, ws):
+        print("--> {} override open <--".format(self.exchange))
+        ws.send(json.dumps({"event":"subscribe","channel": "ticker", "symbol": "tLTCBTC"}))
+
+    def on_message(self, ws, message):
+        print("--> {} override message <--".format(self.exchange))
+        msg = json.loads(message)
+        now = datetime.now().timestamp()*1000
+
+        # determine message type
+        if len(msg[1]) == 2:
+            # heart beat
+            print("Heart beat")
+        elif len(msg[1]) == 10:
+            # ticker stream
+            ticker = {
+                "time": now,
+                "best_ask": msg[1][0],
+                "best_bid": msg[1][2],
+                "last_trade": msg[1][6]
+            }
+            socketio.emit('tick', {'exchange':self.exchange, 'data': ticker, 'type': 'ticker'}, namespace='/ticker')
+
+# setup flask server to host app
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv("SECRET")
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('connect', namespace='/ticker')
+def test_connect():
+    emit('tick', {'data': 'Connected'})
+
+if __name__ == "__main__":
+
+    # create hitbtc instance
+    hitbtc = Hitbtc()
+    hitbtcThread = Thread(target=hitbtc.connect)
+    hitbtcThread.start()
+
+    # create bitfinex instance
+    bitfinex = Bitfinex()
+    bitfinexThread = Thread(target=bitfinex.connect)
+    bitfinexThread.start()
+
+    socketio.run(app)
